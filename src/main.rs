1f0c299f5b54940df02f26d034274b6aa99e05f8
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2024-09-16 21:18:38 +0200

    initial vga buffer

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
+
+static RSNIX: &[u8] = b"RSNIX OS";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    //buffer is located at address 0xb8000
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in RSNIX.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    loop {}
+}
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
     loop {}
 }
