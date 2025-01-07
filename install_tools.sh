bf86a0daa82d106d79ec3ff6882728cdd0dbdbd0
Peter Ducai <32168685+peterducai@users.noreply.github.com>
2025-01-07 23:33:35 +0100

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -4,3 +4,5 @@ rustup override set nightly
 rustup component add llvm-tools-preview
 cargo install cargo-binutils
 rustup target add x86_64-unknown-linux-gnu
+# rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
+# rustup target add aarch64-apple-darwin
